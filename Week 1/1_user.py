3222fda3faca8315d56f500af892c707d9b8378f
Chathusha Mendis <chathushamendis@icloud.com>
2024-09-29 17:35:30 +0530

    Week 1

---
+++ Week 1/1_user.py
@@ -0,0 +1,6 @@
+# Question: Write a program that asks for a user’s name and age and prints a message using both inputs.
+
+# Answer:
+name = input("What's your name? : ")
+age = int(input("What's your age? : "))
+print(f"Hello {name}, you are {age} years old.")
---
+++ Week 1/2_sum.py
@@ -0,0 +1,7 @@
+# Question: Write a program that takes two numbers and prints their sum.
+
+# Answer:
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+sum = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum}.")
